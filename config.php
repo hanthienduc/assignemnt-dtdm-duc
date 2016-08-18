9b88f831a23301007a6b7c1cefcac35a6382c47e
Han Thien Duc <hanthienduc.96@gmail.com>
2016-08-18 14:57:42 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+
+// Edited by ThietkewebX.net
+
+// Cấu hình kết nối dữ liệu
+$hostname     = "mysql.hostinger.vn"; // Tên Host
+$username     = "u383632965_duc"; // Tên user đăng nhập
+$password     = "hanthienduc"; // Mật khẩu đăng nhập
+$databasename = "u383632965_duc"; // Tên cơ sở dữ liệu, nếu không cấu hình đúng, sẽ ra trang trắng.
+
+// Cấu hình hệ thống
+$visitorTimeout = 900; //=15 * 60
+$MAXPAGE = 10;
+$multiLanguage = 1;//0 : single  ;  1 : multi
+
+// Cấu hình ngôn ngữ.
+$arrLanguage = array(
+	array('vn','Việt Nam'),
+	array('en','English')
+);
+
+?>
+
